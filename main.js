01f23f09e1daa46d6500df83541a195723436b5e
louAnFa <louisfalvoiii@icloud.com>
2025-03-20 14:04:19 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-document.querySelectorAll('.description-box').forEach((box) => {
-    box.addEventListener('click', function() {
-        // Toggle 'active' class to expand or collapse the description box
-        this.classList.toggle('active');
-        const price = this.querySelector('.price');
-        price.style.display = this.classList.contains('active') ? 'block' : 'none';
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.item').forEach((item) => {
+        item.addEventListener('click', function() {
+            let link = this.querySelector('.product-title');
+            if (link) {
+                window.open(link.href, '_blank');
+            }
+        });
     });
 });
